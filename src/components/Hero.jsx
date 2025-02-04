232287cc0ee7fcfc570d7b9612d720e66cfe65a5
Shuga8 <lotocharles8@gmail.com>
2025-02-04 20:05:03 +0100

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,82 @@
 import React from "react";
+import monkey from "../assets/monkey.svg";
+import { Link } from "react-router-dom";
+import arrow from "../assets/arrow.svg";
 
 const Hero = () => {
   return (
     <>
-      <section className="hero w-full px-3 md:px-24 lg:px-40 py-4 md:py-16 flex flex-wrap gap-2">
-        <div className="basis-[100%] md:basis-[60%]"></div>
-        <div className="basis-[100%] md:basis-[40%]"></div>
+      <section className="hero w-full px-10 py-4 md:py-16  mt-[-135px] md:mt-[-160px] ">
+        <div className="relative hero-banner py-[100px] md:py-[60px] w-full px-14 text-center md:text-left md:px-44 lg:px-64">
+          <h1 className="text-base text-white mb-2 font-[500]">Hi , I am</h1>
+          <h2 className="text-xl md:text-4xl text-white font-[400]">Athel.</h2>
+          <h3 className="text-xl md:text-7xl text-white font-[650]">Web3</h3>
+          {/* <h3 className="text-xl md:text-7xl ml-0 md:ml-32 text-white font-[650]">
+            Developer
+          </h3> */}
+          <div className="max-w-fit pb-24">
+            <div className="w-[130px] h-[130px] float-left">
+              <img
+                src={monkey}
+                alt=""
+                className="w-[100px] h-full object-contain"
+              />
+            </div>
+            <h3 className="text-xl md:text-7xl float-right text-white font-[650]">
+              Developer
+            </h3>
+          </div>
+
+          <p className="pt-10 pb-6 text-sm text-white tracking-wider">
+            I’m Athel - a Software Developer with expertise in crafting
+            optimized user-friendly Interfaces and User Experiences.
+          </p>
+
+          <Link
+            to={"mailto:Athelathel7@gmail.com"}
+            className="px-10 py-2.5 bg-white border border-white text-[#061A40] text-sm rounded-[25px] uppercase font-[700] hover:bg-transparent hover:text-white transition-colors ease-in-out duration-300"
+          >
+            Hire Me
+          </Link>
+
+          <div className="links absolute right-10 top-[25%] translate-y-[-25%] bg-[#000000] px-4 py-3 rounded-xl hidden md:flex flex-col gap-y-3">
+            <a
+              href="https://t.me/athelking"
+              target="_blank"
+              referrerPolicy="no-refferer no-redirect"
+              className="w-full text-white flex flex-row gap-x-0.5 text-xs place-items-center"
+            >
+              <span>Telegram</span>
+              <img src={arrow} alt="" className="object-contain" />
+            </a>
+            <a
+              href="https://discord.com/users/1085961867157045308"
+              target="_blank"
+              referrerPolicy="no-refferer no-redirect"
+              className="w-full text-white flex flex-row gap-x-0.5 text-xs place-items-center"
+            >
+              <span>Discord</span>
+              <img src={arrow} alt="" className="object-contain" />
+            </a>
+            <a
+              href="mailto:Athelathel7@gmail.com"
+              referrerPolicy="no-refferer no-redirect"
+              className="w-full text-white flex flex-row gap-x-0.5 text-xs place-items-center"
+            >
+              <span>Mail</span>
+              <img src={arrow} alt="" className="object-contain" />
+            </a>
+            <a
+              href="https://x.com/Atheloftomorrow?t=WsZHgVoSB0teeln45ABseQ&s=08"
+              target="_blank"
+              referrerPolicy="no-refferer no-redirect"
+              className="w-full text-white flex flex-row gap-x-0.5 text-xs place-items-center"
+            >
+              <span>X</span>
+              <img src={arrow} alt="" className="object-contain" />
+            </a>
+          </div>
+        </div>
       </section>
     </>
   );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,14 +6,20 @@ import { HiOutlineMenuAlt3 } from "react-icons/hi";
 const Nav = () => {
   return (
     <>
-      <header className="w-full flex justify-between py-5 px-1.5">
-        <div className="logo">
+      <header className="w-full flex justify-between py-5 px-3 md:px-12 relative">
+        <div className="logo w-[130px] h-[140px] bg-tranparent sticky z-[2]">
           <img
             src={logo}
-            className="w-[100px] h-[100px] object-contain"
+            className="w-[150px] h-[150px] object-contain"
             alt=""
           />
         </div>
+
+        <nav>
+          <button className="text-[20px] text-white px-3.5 py-2">
+            <HiOutlineMenuAlt3 />
+          </button>
+        </nav>
       </header>
     </>
   );
